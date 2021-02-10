0c76620c17cc9872aba0134988f06ca5ca6c9576
xcoru <pcd510@hotmail.com>
2021-02-10 17:16:07 -0600

    Create index.js
    Configuración inicial del servidor Node JS.

---
+++ BackEnd/src/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+const { server } = require(path.resolve('src/lib/keys', 'keys'));
+const regional = require(path.resolve('src/lib/utils', 'regional'));
+
+// Initialization ------------------------------------------------ 
+const app = express();
+app.use(cors());
+
+// Settings ------------------------------------------------------
+app.set('port', process.env.PORT || server.port);
+
+// Middlewares ---------------------------------------------------
+app.use(morgan('dev'));
+app.use(express.urlencoded());
+app.use(express.json());
+
+// Routes --------------------------------------------------------                            
+app.use('/api', require('./routes/index.routes'));
+
+
+// Static files --------------------------------------------------
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Control de errores
+app.use(function (req, res) {
+    res.redirect(301, '../');
+});
+
+// Global variables ----------------------------------------------
+
+
+// Starting the server -------------------------------------------
+app.listen(app.get('port'), () => {
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+    console.log("\nFECHA: " + regional.getDateTime() + '\n');
+    console.log('IP SERVIDOR:  ------------------------------------------------\n');
+
+    // Imprime lista de IP's en las que el servidor está escuchando
+    var interfaces = require('os').networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4') {
+                console.log(alias.address + "\t\t" + devName);
+            }
+        }
+    }
+
+    // Muestra el puerto en el que el servidor está escuchando.
+    console.log('\nPORT ', app.get('port'));
+    console.log('--------------------------------------------------------------');
+});
