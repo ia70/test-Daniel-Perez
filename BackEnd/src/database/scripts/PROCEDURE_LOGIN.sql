152c65aa793debd5e1350206a826e7b73e6ef983
xcoru <pcd510@hotmail.com>
2021-02-10 17:32:34 -0600

    Create PROCEDURE_LOGIN.sql
    Procedimiento almacenado para iniciar sesión.

---
+++ BackEnd/src/database/scripts/PROCEDURE_LOGIN.sql
@@ -0,0 +1,14 @@
+DROP PROCEDURE IF EXISTS LOGIN;
+ DELIMITER //
+CREATE PROCEDURE LOGIN(IN usuario VARCHAR(50), IN pass VARCHAR(100))
+	BEGIN
+		SELECT 
+			u.user_id, 
+			CONCAT_WS(" ", p.name, p.first_name) AS "nombre", 
+			u.role_id
+		FROM user u
+		INNER JOIN person p ON u.person_id = p.person_id
+		WHERE u.user_id = usuario AND u.password = pass
+		AND u.state_id = 1 AND p.state_id = 1;
+	END //
+DELIMITER ;
